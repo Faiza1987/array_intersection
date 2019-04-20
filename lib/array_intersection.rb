9d340201dfeec2d954f139ede0eeeb3ca1bcdd1f
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-20 15:25:38 -0700

    Completed array_intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -2,5 +2,22 @@
 # Time complexity: ?
 # Space complexity: ?
 def intersection(array1, array2)
-  raise NotImplementedError
+  # raise NotImplementedError
+  intersections = []
+
+  if array1 == nil || array2 == nil
+    return []
+  end
+
+  for i in (0...array1.length)
+
+    for j in (0...array2.length)
+
+      if array1[i] == array2[j]
+        intersections << array1[i]
+      end # if
+    end # array2
+  end # array1
+  return intersections
 end
+
